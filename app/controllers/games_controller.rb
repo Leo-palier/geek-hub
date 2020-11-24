4aa57e152773b356d00f41e7513087aebf0d57bf
Stéphane Severio <severio.stephane@gmail.com>
2020-11-24 14:48:10 +0100

    cards avec loupe en lien ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,38 @@ class GamesController < ApplicationController
     def index
         @games = Game.all
     end
+
+    def show
+    	@game = Game.find(params[:id])
+    end
+
+    def new
+    	@game = Game.new
+    end
+
+    def create
+    	@game = Game.new(game_params)
+    	if @game.save
+    		redirect_to game_path(@game)
+    	else
+    		render :new
+    	end
+    end
+
+    def edit
+    	@game = Game.find(params[:id])
+    end
+
+    def update
+    	  @game = Game.find(params[:id])
+    @game.update(game_params)
+    
+    redirect_to game_path(@game)
+    end
+
+    private
+
+    def game_params
+  params.require(:game).permit(:name, :year, :console, :price, :photo)
+end
 end
