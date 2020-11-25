76c6bfcf839930593dfbbfd0952c0a9f1a4a5111
Stéphane Severio <severio.stephane@gmail.com>
2020-11-25 13:55:00 +0100

    username done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    before_action :authenticate_user!, :set_user
     def configure_permitted_parameters
         # For additional fields in app/views/devise/registrations/new.html.erb
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
         # For additional in app/views/devise/registrations/edit.html.erb
         devise_parameter_sanitizer.permit(:account_update, keys: [:username])
       end
+
+       private
+
+  def set_user
+    @user = current_user
+  end
 end
