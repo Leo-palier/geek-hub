91d390a8e60ddad6111f259026758c4be82482b7
Stéphane Severio <severio.stephane@gmail.com>
2020-11-26 14:30:52 +0100

    games controller changed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,12 @@
 class GamesController < ApplicationController
 
-    skip_before_action :authenticate_user!, only: [:index, :show]
-
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_game_policy, only: %i[show edit update destroy buy]
 
     def index
-        @games = Game.all
+        @games = policy_scope(Game).order(created_at: :desc)
+    @user = current_user
+        # @games = Game.all
         games_filter = params[:games_filter]
         if games_filter.present?
         if games_filter[:search].present?
@@ -15,43 +17,6 @@ class GamesController < ApplicationController
         end
     end
 
-    def my_games
-        @games = current_user.games
-    end
-
-    def show
-    	@game = Game.find(params[:id])
-    end
-
-    def new
-    	@game = Game.new
-    end
-
-    def create
-    	@game = Game.new(game_params)
-        @game.user = current_user
-    	if @game.save
-    		redirect_to game_path(@game)
-    	else
-    		render :new
-    	end
-    end
-
-    def edit
-    	@game = Game.find(params[:id])
-    end
-
-    def update
-    	  @game = Game.find(params[:id])
-        end
-
-  skip_before_action :authenticate_user!, only: %i[index show]
-  before_action :set_game_policy, only: %i[show edit update destroy buy]
-
-  def index
-    @games = policy_scope(Game).order(created_at: :desc)
-    @user = current_user
-  end
 
   def my_games
     @games = current_user.games
