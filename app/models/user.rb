55fb44e9e1f20b09f6f8985ad25dac6ba4195a5f
Stéphane Severio <severio.stephane@gmail.com>
2020-11-26 16:49:26 +0100

    edituser ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :photo
   has_many :transactions
   has_many :games
   devise :database_authenticatable, :registerable,
